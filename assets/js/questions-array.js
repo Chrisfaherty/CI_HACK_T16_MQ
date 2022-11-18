8cb4684dd659018975944f4aeb571ddf0a5df54f
Pedro Cristo <pcristo35@gmail.com>
2022-11-18 15:47:05 +0000

    Add JS file with the questions array

---
+++ assets/js/questions-array.js
@@ -0,0 +1,89 @@
+// Questions array 
+
+const QUESTIONS_ARRAY = {
+    // Array for the sporst quiz
+    'rock': [
+             {
+                  question: 'WWhat British rock band pioneered the use of the light show?',
+                  choice1: 'Led Zeppelin',
+                  choice2: 'the Beatles',
+                  choice3: 'Pink Floyd',
+                  choice4: 'Lynyrd Skynyrd',
+                  correctAnswer: 3
+                },
+                {
+                  question: 'This British rock group debuted with their album "Mr. Fantasy" (1967):?',
+                  choice1: 'Derek and the Dominos',
+                  choice2: 'The Blur',
+                  choice3: 'Adam and the Ants',
+                  choice4: 'Traffic',
+                  correctAnswer: 4
+                },
+                {
+                  question: 'Which rock band was formed in 1985 by Rose and Stradlin?',
+                  choice1: 'Guns & Roses',
+                  choice2: 'Aerosmith',
+                  choice3: 'Nirvana',
+                  choice4: 'The Eagles',
+                  correctAnswer: 1
+                },
+                  {
+                  question: 'Which instrument did Jimi Hendrix play?',
+                  choice1: 'Piano',
+                  choice2: 'Drum',
+                  choice3: 'Electric Guitar',
+                  choice4: 'Saxophone',
+                  correctAnswer: 3
+                },
+                  {
+                  question: 'Which funk style originated in Washington in the 1970s?',
+                  choice1: 'G-funk',
+                  choice2: 'Go-go',
+                  choice3: 'Afrobeat',
+                  choice4: 'Avant-funk',
+                  correctAnswer: 2
+                },
+                {
+                  question: 'The famous British rock group Led Zeppelin was initially known as:',
+                  choice1: 'The Black Album',
+                  choice2: 'Acca Dacca',
+                  choice3: 'Hollywood Rose',
+                  choice4: 'The New Yardbirds',
+                  correctAnswer: 4
+                },
+                {
+                  question: 'Which member of the Beatles was known as “the quiet Beatle"?',
+                  choice1: 'George Harrison',
+                  choice2: 'John Lennon',
+                  choice3: 'Ringo Starr',
+                  choice4: 'Pete Best',
+                  correctAnswer: 1
+                },
+                {
+                  question: 'What are the instruments in a classic three-piece rock band?',
+                  choice1: 'Guitar, violin, drums',
+                  choice2: 'Guitar, bass, trumpet',
+                  choice3: 'Guitar, bass, drums',
+                  choice4: 'Guitar, organ, drums',
+                  correctAnswer: 3
+                },
+                {
+                  question: 'The song "Born in the U.S.A." (1984) is associated with which singer?',
+                  choice1: 'Clarence Clemons',
+                  choice2: 'Bruce Springsteen',
+                  choice3: 'Steven Van Zandt',
+                  choice4: 'Billy Joel',
+                  correctAnswer: 2
+                },
+                {
+                  question: 'In which year did Psychedelic rock emerge?',
+                  choice1: '1971',
+                  choice2: '1975',
+                  choice3: '1963',
+                  choice4: '1966',
+                  correctAnswer: 4
+                }
+            
+    ],
+    
+};  
