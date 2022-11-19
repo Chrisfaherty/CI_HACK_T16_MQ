36237acc6e61f5eb71cedc5160488b719aab115c
Pedro Cristo <pcristo35@gmail.com>
2022-11-19 09:35:38 +0000

    Add a function for the game end

---
+++ assets/js/game-end.js
@@ -0,0 +1,53 @@
+// Declare const for DOM elements 
+const username = document.getElementById('username');
+const finalScore = document.getElementById('final-score');
+const lastScore = localStorage.getItem('lastScore');
+const saveScore = document.getElementById('save-score');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+/**
+ * Enable the button when the user remove the cursor from the input
+ * */
+username.addEventListener('keyup', () => {
+    saveScore.disabled = !username.value;
+});
+
+// Show to the users the last score 
+finalScore.innerText = lastScore;
+
+/**
+ * Save users last score and username
+ * */
+saveHighScore = (e) => {
+    e.preventDefault();
+
+    const score = {
+        score: lastScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(6);
+
+    // Brings the user to the right page depends on the score made by the user in the last game played
+    if (lastScore == GOLD_TROPHY_SCORE) {
+
+        window.location.assign('trophy-gold.html');
+
+    } else if (lastScore == SILVER_TROPHY_SCORE) {
+
+        window.location.assign('trophy-silver.html');
+
+
+    } else if (lastScore == BRONZE_TROPHY_SCORE) {
+
+        window.location.assign('trophy-bronze.html');
+
+    } else {
+
+        window.location.assign('scores.html');
+
+    }
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+};
