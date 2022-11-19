709afc4a0415326dc0665ec9e6244f823f994681
Pedro Cristo <pcristo35@gmail.com>
2022-11-19 09:31:37 +0000

    Add const for high scores and function to bring the user to a different page after the game is over

---
+++ assets/js/scores.js
@@ -0,0 +1,29 @@
+// Declare const for DOM elements 
+const highScoresList = document.getElementById('highScoresList');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+/**
+* Show to the users the highscores board returning a list with username, score and medal if the user get a score over 199
+* */ 
+highScoresList.innerHTML = highScores
+.map(score => {
+if (score.score == GOLD_TROPHY_SCORE) {
+
+  return `<li class="high-score">${score.name} - ${score.score} - <i class="fas fa-guitar gold-color"></i></li>`;
+  
+} else if (score.score == SILVER_TROPHY_SCORE) {
+
+  return `<li class="high-score">${score.name} - ${score.score} - <i class="fas fa-guitar silver-color"></i></li>`;
+
+} else if (score.score == BRONZE_TROPHY_SCORE) {
+  
+  return `<li class="high-score">${score.name} - ${score.score} - <i class="fas fa-guitar bronze-color"></i></li>`;
+
+} else {
+
+  return `<li class="high-score">${score.name} - ${score.score}`;
+
+}
+
+})
+.join("");
