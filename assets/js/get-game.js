2a095188ea8ae6b76c6a19ef99b80f083b52564b
Pedro Cristo <pcristo35@gmail.com>
2022-11-19 10:15:23 +0000

    Add function to get a random quiz

---
+++ assets/js/get-game.js
@@ -0,0 +1,16 @@
+
+ // Declare const for DOM element 
+ const getQuizBtn = document.getElementById('get-quiz-btn');
+
+// Get a random quiz to play
+ 
+ getQuizBtn.addEventListener('click', () => {
+   
+    let quizPageArray = ['pages/game-1.html', 
+                         'pages/game-2.html', 
+                         'pages/game-3.html'];
+    const QUIZ_PAGE_ARRAY_INDEX = Math.floor(Math.random() * quizPageArray.length);
+    quizPageArray = quizPageArray[QUIZ_PAGE_ARRAY_INDEX];
+  
+    return window.location.assign(quizPageArray);
+  });
