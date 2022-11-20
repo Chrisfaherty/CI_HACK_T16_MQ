38649139b46580f2a9f66d06047b6c44d9933a96
Pedro Cristo <pcristo35@gmail.com>
2022-11-20 17:52:15 +0000

    Add sound effect to the game buttons

--- assets/js/game-fsx.js
+++ assets/js/game-fsx.js
@@ -1,39 +1,41 @@
-
 // Howler game sounds fsx array
 var sfx = {
-    right: new Howl({
-       src: [
-          '../assets/sfx/game/correct.mp3',
-       ]
-    }),
-    wrong: new Howl({
-       src: [
-          '../assets/sfx/game/wronganswer.mp3',
-       ],
-    }),
-    btnSFX: new Howl({
-        src: [
-           './assets/sfx/game/btn-hover.mp3',
-        ]
-    })
-  }
-  
+   right: new Howl({
+      src: [
+         '../assets/sfx/game/correct.mp3',
+      ]
+   }),
+   wrong: new Howl({
+      src: [
+         '../assets/sfx/game/wronganswer.mp3',
+      ],
+   }),
+   btnSFX: new Howl({
+      src: [
+         './assets/sfx/game/btn-hover.mp3',
+      ]
+   }),
+   btnGameSFX: new Howl({
+      src: [
+         '../assets/sfx/game/btn-hover.mp3',
+      ]
+   })
+}
+
 // Control the audio in the game
-    gameAudio = document.getElementById("audio");
-  
-    window.addEventListener('load', () => {
-     gameAudio.volume = 0.2;
-    })
+gameAudio = document.getElementById("audio");
+
+window.addEventListener('load', () => {
+   gameAudio.volume = 0.2;
+})
 
 // Add sound effect to the start game button
-  document.querySelector(".start-game-btn").addEventListener("mouseover", () => {
-    sfx.btnSFX.play();
-  })
+document.querySelector(".game-btn").addEventListener("mouseover", () => {
+   sfx.btnGameSFX.play();
+})
 
-  // Mute audio
-
-  function toggleMute() {
+// Mute audio
+function toggleMute() {
    var myAudio = document.getElementById('audio');
    myAudio.muted = !myAudio.muted;
 }
-
