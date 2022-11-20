b05e2bfc205f696209ddbee04efcd1e45dc0845d
Pedro Cristo <pcristo35@gmail.com>
2022-11-20 19:26:44 +0000

    Add fancy text script animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,32 @@ menuToggle.addEventListener('click', () => {
 menuToggle2.addEventListener('click', () => {
        menuLeft.classList.remove('active')
 });
+
+//  Fancy Text Animation
+const text = document.querySelector('.fancy-text');
+const strText = text.textContent;
+const splitText = strText.split('');
+text.textContent = '';
+
+for (let i = 0; i < splitText.length; i++) {
+    text.innerHTML += '<span>' + splitText[i] + '</span>';
+}
+
+let char = 0;
+let timer = setInterval(onTick, 60);
+
+function onTick() {
+  const span = text.querySelectorAll('span')[char];
+  span.classList.add('fade');
+  char++;
+
+  if(char === splitText.length) {
+      complete();
+      return;
+  }
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+}
